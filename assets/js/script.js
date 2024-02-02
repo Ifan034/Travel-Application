59074c8d25b62daad663cadac52f899a77ab3a8f
Ifan Ul Abedin <82287162+Ifan034@users.noreply.github.com>
2024-02-03 02:20:26 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,6 +105,7 @@ const testimonials = [
     name: "Eva Sawyer",
     job: "Traveller",
     image: "https://randomuser.me/api/portraits/men/32.jpg",
+    rating: "★★★★★",
     testimonial:
       "I stumbled upon this incredible travel website while planning my last-minute adventure, and it turned out to be a hidden gem! The user-friendly interface made it a breeze to navigate through various destinations and find the perfect tour that suited my adventurous spirit. ",
   },
@@ -112,6 +113,7 @@ const testimonials = [
     name: "Katey Topaz",
     job: "Traveller",
     image: "https://randomuser.me/api/portraits/men/22.jpg",
+    rating: "★★★★★",
     testimonial:
       "Planning a family vacation can be a daunting task, but this travel website made it an absolute joy! The website's extensive range of family-friendly destinations and activities helped me create a customized itinerary that catered to everyone's interests.",
   },
@@ -119,12 +121,14 @@ const testimonials = [
     name: "Jae Robin",
     job: "Traveller",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
+    rating: "★★★★★",
     testimonial:
       "As a budget-conscious traveler, I'm always on the lookout for affordable yet quality travel experiences. This website exceeded my expectations! The search filters allowed me to find budget-friendly options without compromising on the adventure.",
   },
   {
     name: "Nicola Blakely",
     job: "Traveller",
+    rating: "★★★★★",
     image: "https://randomuser.me/api/portraits/men/7.jpg",
     testimonial:
       "From affordable accommodations to budget-friendly activities, the website provided a range of choices that fit my pocket. Thanks to this platform, I had an amazing vacation without breaking the bank. I highly recommend it to fellow travelers seeking both value and quality.",
@@ -153,7 +157,8 @@ let displayTestimonial = () => {
   testimonialContainer.innerHTML = `
     <img src=${testimonials[i].image}>
     <h3>${testimonials[i].name}</h3>
-    <h6>${testimonials[i].job}</h6>
+    <h5>${testimonials[i].job}</h5>
+    <h6>${testimonials[i].rating}</h6>
     <p>${testimonials[i].testimonial}</p>
   `;
 };
