61be9b12d0c07cfed34f07817d1931eea2eff23e
Ifan Ul Abedin <82287162+Ifan034@users.noreply.github.com>
2024-03-05 01:01:33 +0600

    first commit

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -6,3 +6,47 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     // Redirect to payment.html
     window.location.href = "payment.html";
   });
+
+
+  /* img slider */
+  let slider = document.querySelector('.slider .list');
+  let items = document.querySelectorAll('.slider .list .item');
+  let next = document.getElementById('next');
+  let prev = document.getElementById('prev');
+  let dots = document.querySelectorAll('.slider .dots li');
+  
+  let lengthItems = items.length - 1;
+  let active = 0;
+  next.onclick = function(){
+      active = active + 1 <= lengthItems ? active + 1 : 0;
+      reloadSlider();
+  }
+  prev.onclick = function(){
+      active = active - 1 >= 0 ? active - 1 : lengthItems;
+      reloadSlider();
+  }
+  let refreshInterval = setInterval(()=> {next.click()}, 5000);
+  function reloadSlider(){
+      slider.style.left = -items[active].offsetLeft + 'px';
+      // 
+      let last_active_dot = document.querySelector('.slider .dots li.active');
+      last_active_dot.classList.remove('active');
+      dots[active].classList.add('active');
+  
+      clearInterval(refreshInterval);
+      refreshInterval = setInterval(()=> {next.click()}, 5000);
+  
+      
+  }
+  
+  dots.forEach((li, key) => {
+      li.addEventListener('click', ()=>{
+           active = key;
+           reloadSlider();
+      })
+  })
+  window.onresize = function(event) {
+      reloadSlider();
+  };
+
+
