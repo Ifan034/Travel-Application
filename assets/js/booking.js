4494ab8581c7ea60b35a17d16528205f400c2dff
Ifan Ul Abedin <82287162+Ifan034@users.noreply.github.com>
2024-03-29 04:22:31 +0600

    first commit

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -64,6 +64,49 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
 
 
 
+//Accordian itenary
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function toggleAccordionText(button) {
+  var accordionText = button.querySelector('.accordion-text');
+
+  if (accordionText.style.color === 'rgb(168, 0, 84)') {
+      accordionText.style.color = '';
+  } else {
+      accordionText.style.color = 'rgb(168, 0, 84)';
+  }
+}
+
+
+//Faq
+function toggleAccordionTextTwo(button) {
+  var accordionTextTwo = button.querySelector('.accordion-text-faq');
+
+  if (accordionTextTwo.style.color === 'rgb(168, 0, 84)') {
+      accordionTextTwo.style.color = '';
+  } else {
+      accordionTextTwo.style.color = 'rgb(168, 0, 84)';
+  }
+}
+
+
+
+
+
+
 
 
 
